c4da32f81830898459ca438fe445c391d713fde5
Yasmine Fortes <yazzcastrof@gmail.com>
2019-11-22 18:37:13 -0300

    "Criando páginas de login e dashboard"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,19 @@
-import React, { useState } from 'react';
-import api from './services/api'
+import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
 
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-    
-    const response = await api.post('/sessions', { email });
-    
-    console.log(response);
-  }
-
+  
   return (
     <div className="container">
     <img src={logo} alt="AirCnC"/>
 
-    <div className="content">
-      <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.</p>
-
-      <form onSubmit = {handleSubmit}>
-        <label htmlFor = "email">E-MAIL *</label>
-        <input type = "email" 
-        id = "email" 
-        placeholder = "Seu principal e-mail"
-        value = {email}
-        onChange = {event => setEmail(event.target.value) }
-        />
-
-        <button className="btn" type="submit">Entrar</button>
-      </form>
-    </div>
+        <div className="content">
+        <Routes />
+     
+      </div>
     </div>
   );
 }
